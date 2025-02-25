b2d9df0ef023817d28e7b5577b922ae31d686ad5
trwngan <ngannie1608@gmail.com>
2025-02-25 07:31:17 +0700

    Update and rename script to script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function login()
 {
-document.getElementById(compiler).style.display="block"
+
   let u = username.value
   let p = password.value 
   if (u=="trwngan" && p="123")
   {
    window.open()
+document.getElementById(compiler).style.display="block"
   }
+else
+ document.write("sai tên đâng nhập hoặc mật khẩu")
 }
